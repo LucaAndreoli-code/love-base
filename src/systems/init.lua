00887219f72bc45e6acc425b9828af77ebe4d586
LucaAndreoli-code <lucandreoli3@gmail.com>
2026-01-26 21:21:47 +0100

    renaming

--- src/systems/init.lua
+++ src/systems/init.lua
@@ -6,12 +6,12 @@ function Systems:load()
     Systems.entity = require("src.systems.entity")
     Logger.info("[Systems] Entity system loaded")
 
-    Systems.entity_manager = require("src.systems.entity_manager")
-    Systems.entity_manager.new()
+    Systems.entityManager = require("src.systems.entity_manager")
+    Systems.entityManager.new()
     Logger.info("[Systems] Entity manager system loaded")
 
-    Systems.state_machine = require("src.systems.state_machine")
-    Systems.state_machine.new()
+    Systems.stateMachine = require("src.systems.state_machine")
+    Systems.stateMachine.new()
     Logger.info("[Systems] State machine system loaded")
 
     Logger.info("[Systems] Done!")
